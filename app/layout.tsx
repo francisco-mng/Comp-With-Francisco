d9d4d4018c4ce7104272485837a57d86f1e6deba
franciscomnopdjh@gmail.com <franciscomnopdjh@gmail.com>
2026-05-20 19:00:12 +0200

    feat: rem db

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        
+
+      </head>
+      <body className="min-h-full flex flex-col">
+        
+        {children}</body>
     </html>
   );
 }
